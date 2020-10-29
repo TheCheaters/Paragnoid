2e73934285e14e80e3a1974aa729f87b46612608
Daniele <daniele.tabanella@stanley-robotics.com>
2020-10-29 22:18:54 +0100

    aggiunta inerzia

--- src/scenes/scene_one.ts
+++ src/scenes/scene_one.ts
@@ -1,23 +1,39 @@
 import {
+  LEFT,
   Physics,
   Scene
 } from 'phaser';
 
-const SPACECRAFT = 'spacecraft';
-const SPACECRAFT_ASSET_PATH = 'assets/spacecraft.png';
+const SPACECRAFT             = 'spacecraft';
+const SPACECRAFT_ASSET_PATH  = 'assets/spacecraft.png';
+const SPACECRAFT_ACC_X_DELTA = 3;
+const SPACECRAFT_DEC_X_DELTA = 1;
+const SPACECRAFT_ACC_Y_DELTA = 3;
+const SPACECRAFT_DEC_Y_DELTA = 1;
 
 enum DIRECTIONS {
   GO_RIGHT = 'GO_RIGHT',
-  GO_LEFT = 'GO_LEFT',
-  GO_UP = 'GO_UP',
-  GO_DOWN = 'GO_DOWN',
+  GO_LEFT  = 'GO_LEFT',
+  GO_UP    = 'GO_UP',
+  GO_DOWN  = 'GO_DOWN',
+}
+
+enum KEYS {
+  UP    = 'UP',
+  RIGHT = 'RIGHT',
+  DOWN  = 'DOWN',
+  LEFT  = 'LEFT',
 }
 
 export default class SceneOne extends Scene {
-  private player ? : Physics.Arcade.Sprite;
-  private cursor ? : Phaser.Types.Input.Keyboard.CursorKeys;
+  private player?: Physics.Arcade.Sprite;
+  private cursor?: Phaser.Types.Input.Keyboard.CursorKeys;
+  private VelocityX = 0;
+  private VelocityY = 0;
+  private lastHorizontalKeyPressed: KEYS.LEFT | KEYS.RIGHT | null = null;
+  private lastVerticalKeyPressed: KEYS.UP | KEYS.DOWN | null = null;
   constructor() {
-    super('scene-one')
+    super('scene-one');
   }
   preload() {
     this.load.spritesheet(SPACECRAFT, SPACECRAFT_ASSET_PATH, {
@@ -72,22 +88,50 @@ export default class SceneOne extends Scene {
   update() {
 
     if (this.player && this.cursor) {
-      this.player.setVelocity(0);
+
+      // ACCELERAZIONE E ANIMAZIONE ORIZONTALE
       if (this.cursor.left?.isDown) {
-        this.player.setVelocityX(-300);
+        this.VelocityX -= SPACECRAFT_ACC_X_DELTA;
         this.player.anims.play(DIRECTIONS.GO_LEFT, true);
+        this.lastHorizontalKeyPressed = KEYS.LEFT;
       } else if (this.cursor.right?.isDown) {
+        this.VelocityX += SPACECRAFT_ACC_X_DELTA;
         this.player.anims.play(DIRECTIONS.GO_RIGHT, true);
-        this.player.setVelocityX(300);
+        this.lastHorizontalKeyPressed = KEYS.RIGHT;
       }
 
+      // ACCELERAZIONE E ANIMAZIONE VERTICALE
       if (this.cursor.up?.isDown) {
-        this.player.setVelocityY(-300);
-        this.player.anims.play(DIRECTIONS.GO_DOWN, true);
-      } else if (this.cursor.down?.isDown) {
-        this.player.setVelocityY(300);
+        this.VelocityY -= SPACECRAFT_ACC_Y_DELTA;
         this.player.anims.play(DIRECTIONS.GO_UP, true);
+        this.lastVerticalKeyPressed = KEYS.UP;
+      } else if (this.cursor.down?.isDown) {
+        this.VelocityY += SPACECRAFT_ACC_Y_DELTA;
+        this.player.anims.play(DIRECTIONS.GO_DOWN, true);
+        this.lastVerticalKeyPressed = KEYS.DOWN;
       }
+
+      // DECELERAZIONE ORIZONTALE
+      if (this.lastHorizontalKeyPressed === KEYS.RIGHT && this.VelocityX > 0) {
+        this.VelocityX -= SPACECRAFT_DEC_X_DELTA;
+      }
+
+      if (this.lastHorizontalKeyPressed === KEYS.LEFT && this.VelocityX < 0) {
+        this.VelocityX += SPACECRAFT_DEC_X_DELTA;
+      }
+
+      // DECELERAZIONE VERTICALE
+      if (this.lastVerticalKeyPressed === KEYS.DOWN && this.VelocityY > 0) {
+        this.VelocityY -= SPACECRAFT_DEC_Y_DELTA;
+      }
+
+      if (this.lastVerticalKeyPressed === KEYS.UP && this.VelocityY < 0) {
+        this.VelocityY += SPACECRAFT_DEC_Y_DELTA;
+      }
+
+      // SPOSTAMENTO SPRITE
+      this.player.setVelocityX(this.VelocityX);
+      this.player.setVelocityY(this.VelocityY);
     }
   }
 }
