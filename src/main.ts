fc4293aa5eeafdff0cbdf09ed535292c8954de8e
Dario Costa <costa.dario@outlook.it>
2020-11-04 00:47:59 +0100

    modifica

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import Space from '~/scenes/space-background';
 
 const config: Phaser.Types.Core.GameConfig & { pixelArt: boolean } = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	width: 1600,
+	height: 1200,
 	pixelArt: true,
 	physics: {
 		default: 'arcade',
