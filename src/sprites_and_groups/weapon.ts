95e57551cf33731514c3287fb52a454bec1b5e3f
Daniele <daniele.tabanella@stanley-robotics.com>
2020-11-15 23:27:55 +0100

    Co-authored-by: Giambattista Martino <johnmyung@tiscalinet.it>

--- src/sprites_and_groups/weapon.ts
+++ src/sprites_and_groups/weapon.ts
@@ -1,26 +1,31 @@
 import { Scene } from "phaser";
-import { AUDIO_MISSILE, MISSILE } from '~/constants.json';
+import { DEFAULT } from '~/sprites_and_groups/weapons_types.json';
+import WEAPON_TYPES from '~/sprites_and_groups/weapons_types.json';
+
+type WeaponType = keyof typeof WEAPON_TYPES;
 
 export class Weapon extends Phaser.Physics.Arcade.Sprite {
-  energy = 90;
+
+  DAMAGE = DEFAULT.DAMAGE;
+  FIRE_SPEED = DEFAULT.FIRE_SPEED;
+  TEXTURE_NAME = DEFAULT.TEXTURE_NAME;
+  SPRITE_ASSET_PATH = DEFAULT.SPRITE_ASSET_PATH;
+  AUDIO_NAME = DEFAULT.AUDIO_NAME;
+  AUDIO_ASSET_PATH = DEFAULT.AUDIO_ASSET_PATH;
+  WIDTH = DEFAULT.WIDTH;
+  HEIGHT = DEFAULT.HEIGHT;
+
   constructor(scene: Scene, x: number, y: number) {
-    super(scene, x, y, MISSILE);
+    super(scene, x, y, DEFAULT.TEXTURE_NAME);
   }
 
-  fire(x: number, y: number, type: string, fireSpeed: number) {
+  fire(x: number, y: number) {
     this.body.enable = true;
     this.body.reset(x + 2, y + 20);
-
     this.setActive(true);
     this.setVisible(true);
-
-    if (type === 'player') {
-      this.setVelocityX(1000);
-    } else {
-      this.setVelocityX(-fireSpeed);
-    }
-
-    this.scene.sound.play(AUDIO_MISSILE);
+    this.setVelocityX(this.FIRE_SPEED);
+    this.scene.sound.play(this.AUDIO_NAME);
   }
 
   kill() {
@@ -29,19 +34,37 @@ export class Weapon extends Phaser.Physics.Arcade.Sprite {
     this.setVisible(false);
   }
 
+  setWeaponTexture(texture: string) {
+    this.setTexture(texture);
+  }
+
 	preUpdate(time: number, delta: number) {
 		super.preUpdate(time, delta);
 
-		if (this.x > this.scene.scale.width || this.x < -200) {
-      this.kill();
-		}
+		if (this.x > this.scene.scale.width || this.x < -200) this.kill();
 	}
 }
 
 export class PlayerWeapon extends Weapon {
-  energy = 90;
+  constructor(scene: Scene, x: number, y: number) {
+    super(scene, x, y);
+    this.DAMAGE = DEFAULT.DAMAGE;
+    this.FIRE_SPEED = DEFAULT.FIRE_SPEED;
+    this.TEXTURE_NAME = DEFAULT.TEXTURE_NAME;
+    this.SPRITE_ASSET_PATH = DEFAULT.SPRITE_ASSET_PATH;
+    this.AUDIO_NAME = DEFAULT.AUDIO_NAME;
+    this.AUDIO_ASSET_PATH = DEFAULT.AUDIO_ASSET_PATH;
+    this.WIDTH = DEFAULT.WIDTH;
+    this.HEIGHT = DEFAULT.HEIGHT;
+  }
 }
 
 export class EnemyWeapon extends Weapon {
-  energy = 90;
+  fire(x: number, y: number, weaponType?: WeaponType) {
+    if (weaponType) {
+      this.setWeaponTexture(WEAPON_TYPES[weaponType].TEXTURE_NAME);
+      this.FIRE_SPEED = (WEAPON_TYPES[weaponType].FIRE_SPEED);
+    }
+    super.fire(x, y);
+  }
 }
