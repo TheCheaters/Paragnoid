6849ed8670a34d7c17e6bb8803cd652c0e391863
scorpionkkk <johnmyung@tiscalinet.it>
2020-11-20 00:12:06 +0100

    inserito oggetto PowerUp

---
+++ src/sprites_and_groups/powerup.ts
@@ -0,0 +1,10 @@
+import * as C from '~/constants.json';
+import { KEYS, DIRECTIONS } from '~/globals';
+import { SPACECRAFT } from '~/constants.json';
+
+import Game from '../scenes/game';
+import { Scene } from 'phaser';
+
+export default class PowerUp extends Phaser.Physics.Arcade.Sprite {
+
+}
