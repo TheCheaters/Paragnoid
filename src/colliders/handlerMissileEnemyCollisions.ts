238961c61e10652daa3a4ee984fb19d920c3cd5d
Daniele <daniele.tabanella@stanley-robotics.com>
2020-11-19 21:28:48 +0100

    fix

--- src/colliders/handlerMissileEnemyCollisions.ts
+++ src/colliders/handlerMissileEnemyCollisions.ts
@@ -8,15 +8,13 @@ export default (gameScene: Game) => (enemy: Enemy, weapon: PlayerWeapon ) => {
   const {x, y} = enemy;
   enemy.energy = enemy.energy - weapon.DAMAGE;
   weapon.kill();
-  if (enemy.energy >0) {
-    gameScene.sound.add (HIT_ENEMY, { loop: false }).play();
-  }
-  else { 
   if (enemy.energy <= 0) {
     gameScene.score += enemy.getData('score');
     gameScene.scoreText.setText(`Score: ${gameScene.score}`);
     gameScene.explosions?.addExplosion(x, y);
     gameScene.sound.add(AUDIO_EXPLOSION, { loop: false }).play();
     enemy.kill();
+  } else {
+    gameScene.sound.add (HIT_ENEMY, { loop: false }).play();
   }
 }
