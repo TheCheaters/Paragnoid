6caed3be5916d498b4f8a929b532e85e2d5bef76
Daniele <daniele.tabanella@stanley-robotics.com>
2021-01-11 22:56:10 +0100

    aggiunti componenti per armi player

--- src/sprites/player/player.ts
+++ src/sprites/player/player.ts
@@ -1,5 +1,6 @@
 import { DIRECTIONS } from '~/globals';
-import { SPACECRAFT, RESPAWN_TIME, MORTAL, COMPONENTS } from '~/constants.json';
+import { SPACECRAFT, RESPAWN_TIME, MORTAL } from '~/constants.json';
+import components from '~/sprites/player/components_types.json';
 import WEAPON_PLAYER_TYPES from '~/sprites/player/weapons_player_types.json';
 import { PowerUpTypes, PowerUpType } from '~/sprites/powerups/powerups';
 
@@ -8,7 +9,7 @@ import debug from '~/utils/debug';
 import Lifeline from '~/utils/Lifeline';
 import { WeaponPlayerType } from '~/types/weapons';
 
-const weaponNames = Object.keys(WEAPON_PLAYER_TYPES);
+const weaponNames = Object.keys(WEAPON_PLAYER_TYPES) as WeaponPlayerType[];
 
 export default class Player extends Phaser.Physics.Arcade.Sprite {
 
@@ -17,15 +18,18 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
   public maxEnergy!: number;
   private lifeLine!: Lifeline;
 
-  public weaponType = weaponNames[0] as WeaponPlayerType;
-  public weaponLevel = 0;
-  private test: Phaser.GameObjects.Image;
+  public weaponType!: WeaponPlayerType;
+  public weaponLevel!: number;
+  private cannon!: Phaser.GameObjects.Image;
+  private cannonPosX!: number;
+  private cannonPosY!: number;
+  private cannonfireX!: number;
+  private cannonfireY!: number;
 
   constructor(scene: Game, x: number, y: number, texture: string) {
     super(scene, x, y, texture);
 
     scene.add.existing(this);
-    this.test = scene.add.image(this.x, this.y, COMPONENTS, 'Comp_9.png').setScale(0.2);
     scene.physics.add.existing(this);
     this.setCollideWorldBounds(true);
     this.setImmovable(true);
@@ -77,10 +81,31 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
       frameRate: 20
     });
 
-
+    this.setInitialEnergy();
+    this.setInitialWeapon();
+    this.setCannonComponent();
     // BEHAVIOR
-    this.maxEnergy = this.energy;
     // this.lifeLine = new Lifeline(this.scene as Game, this);
+  }
+
+  setInitialWeapon() {
+    this.weaponType = 'DEFAULT';
+    this.weaponLevel = 0;
+  }
+
+  setInitialEnergy() {
+    this.maxEnergy = this.energy;
+  }
+
+  setCannonComponent() {
+    const { TEXTURE_NAME, FRAME_NAME } = WEAPON_PLAYER_TYPES[this.weaponType].LEVELS[this.weaponLevel];
+    const { POS_X, POS_Y, FIRE_X, FIRE_Y, SCALE } = components[FRAME_NAME];
+    this.cannonPosX = POS_X;
+    this.cannonPosY = POS_Y;
+    this.cannonfireX = FIRE_X;
+    this.cannonfireY = FIRE_Y;
+    this.cannon = this.scene.add.image(this.cannonPosX + this.x, this.cannonPosY + this.y, TEXTURE_NAME, FRAME_NAME);
+    this.cannon.setScale(SCALE);
   }
 
   takeHit(damage: number) {
@@ -113,10 +138,10 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
   }
 
   changeWeapon() {
-    this.weaponType = weaponNames[Phaser.Math.Between(0, weaponNames.length - 1)] as WeaponPlayerType;
+    this.weaponType = weaponNames[Phaser.Math.Between(0, weaponNames.length - 1)];
   }
 
-  increaseLevelWeapon(){
+  increaseLevelWeapon() {
     if (this.weaponLevel < WEAPON_PLAYER_TYPES[this.weaponType].LEVELS.length - 1) {this.weaponLevel += 1;}
   }
 
@@ -128,7 +153,7 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     this.weaponType = weaponNames[type] as WeaponPlayerType;
   }
 
-   updgradeWeapon() {
+  updgradeWeapon() {
     const scene = this.scene as Game;
     if (this.weaponLevel < WEAPON_PLAYER_TYPES[this.weaponType].LEVELS.length - 1) {
       this.weaponLevel += 1;
@@ -202,8 +227,8 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     super.preUpdate(time, delta);
 
     const scene = this.scene as Game;
-    this.test.setPosition(this.x, this.y);
     scene.shield.moveShield(this.x, this.y);
+    this.cannon.setPosition(this.cannonPosX + this.x, this.cannonPosY + this.y);
     // this.lifeLine.update();
 
   }
--- src/types/weapons.ts
+++ src/types/weapons.ts
@@ -1,13 +1,15 @@
 import WEAPON_SATELLITE_TYPES from '~/sprites/satellites/weapons_satellite_types.json';
 import WEAPON_ENEMY_TYPES from '~/sprites/enemies/weapons_enemy_types.json';
 import WEAPON_PLAYER_TYPES from '~/sprites/player/weapons_player_types.json';
-export type WeaponPlayerType = keyof typeof WEAPON_PLAYER_TYPES;
-export type WeaponEnemyType = keyof typeof WEAPON_ENEMY_TYPES;
+export type WeaponPlayerType    = keyof typeof WEAPON_PLAYER_TYPES;
+export type WeaponEnemyType     = keyof typeof WEAPON_ENEMY_TYPES;
 export type WeaponSatelliteType = keyof typeof WEAPON_SATELLITE_TYPES;
-export type WeaponTypeName = WeaponPlayerType | WeaponEnemyType | WeaponSatelliteType;
+export type WeaponTypeName      = WeaponPlayerType | WeaponEnemyType | WeaponSatelliteType;
 
 export type Level = {
-  ANGLE: number[],
+  ANGLE:               number[],
+  TEXTURE_NAME:        string;
+  FRAME_NAME:          string;
   GRAVITY_X:           number;
   GRAVITY_Y:           number;
   VERTICAL_OFFSET:     number;
@@ -17,18 +19,18 @@ export type Level = {
 }
 
 export type WeaponType = {
-  DAMAGE: number;
-  FIRE_SPEED: number;
-  TEXTURE_NAME: string;
-  FRAME_NAME: string;
-  AUDIO_NAME: string;
+  DAMAGE:           number;
+  FIRE_SPEED:       number;
+  TEXTURE_NAME:     string;
+  FRAME_NAME:       string;
+  AUDIO_NAME:       string;
   AUDIO_ASSET_PATH: string;
-  WIDTH: number;
-  HEIGHT: number;
-  SCALE: number;
-  EXPLODES: boolean;
-  PARTICLES: string;
-  FOLLOW: boolean;
+  WIDTH:            number;
+  HEIGHT:           number;
+  SCALE:            number;
+  EXPLODES:         boolean;
+  PARTICLES:        string;
+  FOLLOW:           boolean;
 }
 
 export type PlayerWeaponType = WeaponType & {
