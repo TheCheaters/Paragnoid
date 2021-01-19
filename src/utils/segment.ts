c954996c9e4c47d224b7466607d5dca5c1c5eb22
Daniele <daniele.tabanella@stanley-robotics.com>
2021-01-19 22:25:36 +0100

    fix

--- src/utils/segment.ts
+++ src/utils/segment.ts
@@ -1,24 +1,26 @@
-import {
-   Scene
-} from "phaser";
-import {
-   PV_FONT_NAME
-} from '~/constants.json';
+import Game from "~/scenes/game";
 
-export default class Segment extends Phaser.GameObjects.Line {
+export default class Segment {
+   public scene!: Game;
    public level!: number
    public startX!: number
    public startY!: number
    public endX!: number
    public endY!: number
    public scoreText!: Phaser.GameObjects.DynamicBitmapText;
-   constructor(scene: Scene, startX: number, startY: number, endX: number, endY: number, level: number) {
-      super(scene, startX, startY, endX, endY);
+   private style: Phaser.GameObjects.Graphics;
+   private line: Phaser.Geom.Line;
+
+   constructor(scene: Game, startX: number, startY: number, endX: number, endY: number, level: number) {
+      this.scene = scene;
       this.startX = startX;
       this.startY = startY;
       this.endX = endX;
       this.endY = endY;
       this.level = level;
+      this.style = this.scene.add.graphics({ lineStyle: { width: 5, color: 0x00ff3d } });
+      this.line = new Phaser.Geom.Line();
+
    }
 
    // punto medio non serve si può usare la funzione GetMidPoint
@@ -27,13 +29,19 @@ export default class Segment extends Phaser.GameObjects.Line {
    }
 
    draw() {
-      const graphics = new Phaser.GameObjects.Graphics(this.scene);
-      graphics.lineBetween(this.startX, this.startY, this.endX, this.endY);
+      // const graphics = new Phaser.GameObjects.Graphics(this.scene);
+      // graphics.lineBetween(this.startX, this.startY, this.endX, this.endY);
       // this.scoreText = this.scene.add.dynamicBitmapText(260, 260, PV_FONT_NAME, this.startX.toString(), 14);
-      graphics.lineStyle(10, 0xFF0000)
-      graphics.beginPath();
-      console.log(this.startX, this.scaleY, this.endX, this.endY);
+      // graphics.lineStyle(10, 0xFF0000)
+      console.log(this.startX, this.startY, this.endX, this.endY);
+      console.log(this.scene);
       // console.log(graphics);
+      this.line.x1 = this.startX;
+      this.line.y1 = this.startY;
+      this.line.x2 = this.endX;
+      this.line.y2 = this.endY;
+      this.style.strokeLineShape(this.line);
+
    }
 
 }
