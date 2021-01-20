37e93684ff0979f9a4e5218ce2a0674328bfc067
scorpionkkk <johnmyung@tiscalinet.it>
2021-01-20 23:18:46 +0100

    rimozione primo segmento

--- src/sprites/weapons/lampo.ts
+++ src/sprites/weapons/lampo.ts
@@ -21,26 +21,36 @@ export default class Lampo {
     generazione(startPointX: number, startPointY: number, endPointX: number, endPointY: number) {
         const listaSegmenti: Segment[] = []; //array vuoto
         // const nuovaLista: Segment[] = []; //un altro array di appoggio
-        listaSegmenti.push(new Segment(this.scene, startPointX, startPointY, endPointX, endPointY, 1)); //metto nell'array un primo segmento che va dal punto iniziale a quello finale
+        let segmento1 = new Segment(this.scene, startPointX, startPointY, endPointX, endPointY, 1);
+        listaSegmenti.push(segmento1); //metto nell'array un primo segmento che va dal punto iniziale a quello finale
         let offsetSegmento = this.maxOffset; //il massimo Offset che posso dare ad un vertice del segmento
 
         for (let i = 0; i < this.generazioni; i++) {
             console.log('ciclo 1');
-            for (let i = 0; i < this.generazioni; i++) {
-            // for (const vecchioSegmento of listaSegmenti) { //per tutti gli elementi del primo array
+            //for (let i = 0; i < this.generazioni; i++) {
+            for (const oldsegment in listaSegmenti) { //per tutti gli elementi del primo array
                 console.log('ciclo 2');
                 // console.log(vecchioSegmento);
-                const segmento = new Segment(this.scene, startPointX, startPointY, endPointX, endPointY, 1); // fai un altro segmento
+                //const segmento = new Segment(this.scene, startPointX, startPointY, endPointX, endPointY, 1); // fai un altro segmento
+                listaSegmenti.pop();
                 let puntoMedioX = Phaser.Math.Average([startPointX, endPointX]); //calcola il punto medio delle coordinate X dei punti iniziale e finale
+                console.log(puntoMedioX, startPointX, endPointX);
                 let puntoMedioY = Phaser.Math.Average([startPointY, endPointY]); //calcola il punto medio delle coordinate Y dei punti iniziale e finale
+                console.log(puntoMedioY, startPointY, endPointY);
 
                 //trascina il punto medio per un estensione casuale lungo la normale al segmento
-                const angolo = Math.atan2(segmento.endY - segmento.startY, segmento.endX - segmento.startX); //calcolo angolo di inclinazione del segmento
+                const angolo = Math.atan2(endPointY - startPointY, endPointX - startPointX); //calcolo angolo di inclinazione del segmento
+                console.log(angolo);
                 const randomOffset = Phaser.Math.RND.between(-offsetSegmento, offsetSegmento); // prendi un offset random tra quelli massimi negativo e positivo
+                console.log(randomOffset);
                 const x1 = Math.sin(angolo) * randomOffset + puntoMedioX;
+                console.log(x1);
                 const y1 = -Math.cos(angolo) * randomOffset + puntoMedioY;
+                console.log(y1);
                 const x2 = -Math.sin(angolo) * randomOffset + puntoMedioX;
+                console.log(x2);
                 const y2 = Math.cos(angolo) * randomOffset + puntoMedioY;
+                console.log(y2);
 
                 if (Phaser.Math.RND.between(-1, 1) < 0) { //sceglie tra una estensione a sx o a dx
                     puntoMedioX = x1;
@@ -49,14 +59,15 @@ export default class Lampo {
                     puntoMedioX = x2;
                     puntoMedioY = y2;
                 }
-                listaSegmenti.push(new Segment(this.scene, segmento.startX, segmento.startY, puntoMedioX, puntoMedioY, segmento.level));
-                listaSegmenti.push(new Segment(this.scene, puntoMedioX, puntoMedioY, segmento.endX, segmento.endY, segmento.level));
+                listaSegmenti.push(new Segment(this.scene, startPointX, startPointY, puntoMedioX, puntoMedioY, 1));
+                listaSegmenti.push(new Segment(this.scene, puntoMedioX, puntoMedioY, endPointX, endPointY, 1));
+                
 
                 if (i === 0 || i === 2) {
-                    const distanza = Math.sqrt(Math.pow(puntoMedioX - segmento.startX, 2) + Math.pow(puntoMedioY - segmento.startY, 2));
+                    const distanza = Math.sqrt(Math.pow(puntoMedioX - startPointX, 2) + Math.pow(puntoMedioY - startPointY, 2));
                     const endPointDiviso = {
-                        x: segmento.endX,
-                        y: segmento.endY
+                        x: endPointX,
+                        y: endPointY
                     };
                     let angoloSuddivisione;
                     if (Phaser.Math.RND.between(0, 2) < 1) {
@@ -66,7 +77,7 @@ export default class Lampo {
                     }
                     Phaser.Math.RotateAroundDistance(endPointDiviso, puntoMedioX, puntoMedioY, angoloSuddivisione, this.scala * distanza);
 
-                    listaSegmenti.push(new Segment(this.scene, puntoMedioX, puntoMedioY, endPointDiviso.x, endPointDiviso.y, segmento.level + 1));
+                    listaSegmenti.push(new Segment(this.scene, puntoMedioX, puntoMedioY, endPointDiviso.x, endPointDiviso.y, 1 + 1));
 
                 }
 
